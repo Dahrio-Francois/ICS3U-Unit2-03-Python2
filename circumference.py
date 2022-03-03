a155bbc0f983987f4c977ba686022022506c8f64
Dahrio-Francois <dahrio.francois@mths.ca>
2022-03-03 00:04:10 +0000

    added circumference.py and constants.py

---
+++ circumference.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Dahrio Francois
+# Created on: March 2022
+# This program calculates the circumference of a circle
+#   with user inputted radius
+
+
+import constants
+
+
+def main():
+    # this function calculates the circumference
+
+    # input
+    radius = int(input("Enter the radius of the circle (mm): "))
+
+    # process
+    circumference = constants.TAU * radius
+
+    # output
+    print("")
+    print("Circumference is {0} mm².".format(circumference))
+    print("\nDone")
+
+
+if __name__ == "__main__":
+    main()
